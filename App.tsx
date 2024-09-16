69bebdca50a2044f1c018f3701360ac0b91e3e2c
Minh <0942051066minh@gmail.com>
2024-09-16 22:57:43 +0700

    View, Text

--- App.tsx
+++ App.tsx
@@ -4,11 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Minh</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.header}>Hello Minh</Text>
+        <Text>Minh hihihi</Text>
+      </View>
+      <Text style={styles.helll1}> Hello world 1</Text>
+      <Text>Hello world 2</Text>
     </View>
   );
 }
+
+
+//css in js
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +23,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  helll1: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    borderWidth: 1,
   },
 });
